86214cd197c341c1dc1416c1b3d54930d49c9863
fatedavi <achmaddviii@gmail.com>
2026-05-25 13:32:48 +0700

    fix: update output directory in vercel.json and add server preset in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  tanstackStart: {
+    server: { 
+      preset: "vercel"
+    },
+  },
   vite: {
     resolve: {
       alias: {
